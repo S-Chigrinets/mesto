7c3e01d3670dfae7136dcd660ccc6a4596454578
S-Cigrinets <s.chigrinets@ya.ru>
2021-08-05 15:04:34 +0300

    fix: fixed bugs with adaptive

--- scripts/index.js
+++ scripts/index.js
@@ -4,8 +4,8 @@ const popupButtonClose = document.querySelector('.popup__button-close');
 // кнопка закрытия попапа редактирования "имя" и "о себе"
 const popup = document.querySelector('.popup');
 
-let popupText = document.querySelector('.popup__input_text');
-let popupName = document.querySelector('.popup__input_name');
+let popupText = document.querySelector('.popup__input_text_about-myself');
+let popupName = document.querySelector('.popup__input_text_name');
 let popupForm = document.querySelector('.popup__form');
 let profileText = document.querySelector('.profile__input-about-me');
 let profileName = document.querySelector('.profile__input-name');
